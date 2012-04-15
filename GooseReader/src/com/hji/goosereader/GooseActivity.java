84d751f9730e31da4d77aade266a7e59766968d2
joneshf <joneshf@imail.losrios.edu>
2012-04-14 18:08:22 -0700

    Minor NPE when backing out before the first load.

--- GooseReader/src/com/hji/goosereader/GooseActivity.java
+++ GooseReader/src/com/hji/goosereader/GooseActivity.java
@@ -57,7 +57,7 @@ import com.hardyjones.goosereader.R;
  * @author Hardy Jones III
  */
 public class GooseActivity extends Activity {
-	
+
 	private static final int SHOW_INFO_DIALOG = 0;
 	private static final int FIRST_COMIC_NUMBER = 1;
 	private static final Random sRandomNumber = new Random();
@@ -72,47 +72,47 @@ public class GooseActivity extends Activity {
 	private static String sComicTitle;
 	private static String sImageUrl;
 	private static String sPresentUrl;
-	
+
 	private static Dialog sInfoDialog;
 	private static Document sRawHtml;
 	private static LinearLayout sNavigationLayout;
 	private static ProgressDialog sLoadingComic; 
 	private static TextView sAltTextView;
-    private static WebView sComicView;
-	
-    private static TemporaryInfo oldInfo;
-    
-    /**
-     * Starts {@link scrapeSiteTask} in another thread to load the comic into the webview.
-     */
-    private void loadComic() {
-    	// Create an anonymous scrape task in another thread.
-    	new scrapeSiteTask(this).execute();
-    }
+	private static WebView sComicView;
 
-    /**
-     * Handles each button in the navigation layout.
-     * @param button The button that performed the event.
-     */
+	private static TemporaryInfo oldInfo;
+
+	/**
+	 * Starts {@link scrapeSiteTask} in another thread to load the comic into the webview.
+	 */
+	private void loadComic() {
+		// Create an anonymous scrape task in another thread.
+		new scrapeSiteTask(this).execute();
+	}
+
+	/**
+	 * Handles each button in the navigation layout.
+	 * @param button The button that performed the event.
+	 */
 	public void navigationButtonHandler(View button) {
 		button.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-		
+
 		// Save the old info in case loading is canceled mid-load.
 		oldInfo = new TemporaryInfo(sPresentComicNumber, sCurrentComicNumber, sAltText,
 				sComicTitle, sImageUrl, sPresentUrl);
-		
+
 		switch (button.getId()) {
 		case R.id.firstButton:
 			sPresentComicNumber = FIRST_COMIC_NUMBER;
 			break;
-		// Don't go past the beginning.
+			// Don't go past the beginning.
 		case R.id.previousButton:
 			sPresentComicNumber -= (FIRST_COMIC_NUMBER == sPresentComicNumber) ? 0 : 1;
 			break;
 		case R.id.randomButton:
 			sPresentComicNumber = sRandomNumber.nextInt(sCurrentComicNumber);
 			break;
-		// Don't go past the end.
+			// Don't go past the end.
 		case R.id.nextButton:
 			sPresentComicNumber += (sCurrentComicNumber == sPresentComicNumber) ? 0 : 1;
 			break;
@@ -128,165 +128,165 @@ public class GooseActivity extends Activity {
 		// Load the new comic number.
 		loadComic();
 	}
-	
+
 	/**
 	 * Called when the activity is first created.
 	 */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        /* Set up the webview */
-        sComicView = (WebView) findViewById(R.id.comicView);
-        sComicView.getSettings().setBuiltInZoomControls(true);
-        sComicView.getSettings().setLoadWithOverviewMode(true);
-        sComicView.getSettings().setUseWideViewPort(true);
-        sComicView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        sComicView.setOnLongClickListener(new OnLongClickListener() {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		/* Set up the webview */
+		sComicView = (WebView) findViewById(R.id.comicView);
+		sComicView.getSettings().setBuiltInZoomControls(true);
+		sComicView.getSettings().setLoadWithOverviewMode(true);
+		sComicView.getSettings().setUseWideViewPort(true);
+		sComicView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+		sComicView.setOnLongClickListener(new OnLongClickListener() {
 			public boolean onLongClick(View v) {
 				showDialog(SHOW_INFO_DIALOG);
 
 				return true;
 			}
 		});
-        sComicView.setWebViewClient(new WebViewClient() {
-        	
-        	@Override
-        	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        		view.loadUrl(url);
-        		return true;
-        	}
-        	@Override
-        	public void onPageFinished(WebView view, String url) {
-        		if (null != sLoadingComic && sLoadingComic.isShowing()) {
-        			sLoadingComic.dismiss();
-        		}
-        	}
-        });
+		sComicView.setWebViewClient(new WebViewClient() {
 
-        /* Set up the navigation control layout if it is to be hidden. */
-        sNavigationLayout = (LinearLayout) findViewById(R.id.buttonLayout);
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			}
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				if (null != sLoadingComic && sLoadingComic.isShowing()) {
+					sLoadingComic.dismiss();
+				}
+			}
+		});
+
+		/* Set up the navigation control layout if it is to be hidden. */
+		sNavigationLayout = (LinearLayout) findViewById(R.id.buttonLayout);
 		// Make a new progress dialog.
 		sLoadingComic = new ProgressDialog(this);
 		// Load it up with the loading info.
 		sLoadingComic.setMessage(getString(R.string.loading_comic));
 
-        // Load the latest comic.
-        loadComic();
-    }
-    
-    /**
-     * Creates a dialog for the alt text.
-     */
-    @Override
-    protected Dialog onCreateDialog(int id) {
-    	switch (id) {
-    	case SHOW_INFO_DIALOG:
-    		sInfoDialog = new Dialog(this);
-    		sInfoDialog.setContentView(R.layout.alt_text);
-    		sAltTextView = (TextView) sInfoDialog.findViewById(R.id.altTextView);
-    		
-    		return sInfoDialog;
-    	default:
-    		break;
-    	}
-    	return super.onCreateDialog(id);
-    }
-    
-    /**
-     * Creates a menu when the menu button is pressed on the device.
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	MenuInflater mainMenu = getMenuInflater();
-    	mainMenu.inflate(R.menu.menu, menu);
+		// Load the latest comic.
+		loadComic();
+	}
 
-    	return true;
-    }
+	/**
+	 * Creates a dialog for the alt text.
+	 */
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case SHOW_INFO_DIALOG:
+			sInfoDialog = new Dialog(this);
+			sInfoDialog.setContentView(R.layout.alt_text);
+			sAltTextView = (TextView) sInfoDialog.findViewById(R.id.altTextView);
 
-    /** 
-     * Chooses which options menu selection was pressed.
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-    	case R.id.toggleNavigation:
-    		// Flip the boolean.
-    		sNavigation = !sNavigation;
-    		// Display/hide the nav.
-    		toggleNavigation();
-    		break;
-    	case R.id.info:
-    		// Bring up the alt-text.
-    		showDialog(SHOW_INFO_DIALOG);
-    		break;
-    	case R.id.refresh:
-    		// Load the same comic again.
-    		loadComic();
-    		break;
-    	case R.id.share:
-    		// Start the share intent.
-    		shareComic();
-    		break;
-    	default:
-    		break;
-    	}
-    	
-    	return true;
-    }
-    
-    /** 
-     * Shows additional information including: comic title and alt text.
-     * @param id ID of the dialog to prepare.
-     * @param dialog The dialog to prepare.
-     */
-    @Override
-    protected void onPrepareDialog (int id, Dialog dialog) {
-    	switch (id) {
-    	case SHOW_INFO_DIALOG:
-    		sInfoDialog.setTitle(sComicTitle);
+			return sInfoDialog;
+		default:
+			break;
+		}
+		return super.onCreateDialog(id);
+	}
+
+	/**
+	 * Creates a menu when the menu button is pressed on the device.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater mainMenu = getMenuInflater();
+		mainMenu.inflate(R.menu.menu, menu);
+
+		return true;
+	}
+
+	/** 
+	 * Chooses which options menu selection was pressed.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.toggleNavigation:
+			// Flip the boolean.
+			sNavigation = !sNavigation;
+			// Display/hide the nav.
+			toggleNavigation();
+			break;
+		case R.id.info:
+			// Bring up the alt-text.
+			showDialog(SHOW_INFO_DIALOG);
+			break;
+		case R.id.refresh:
+			// Load the same comic again.
+			loadComic();
+			break;
+		case R.id.share:
+			// Start the share intent.
+			shareComic();
+			break;
+		default:
+			break;
+		}
+
+		return true;
+	}
+
+	/** 
+	 * Shows additional information including: comic title and alt text.
+	 * @param id ID of the dialog to prepare.
+	 * @param dialog The dialog to prepare.
+	 */
+	@Override
+	protected void onPrepareDialog (int id, Dialog dialog) {
+		switch (id) {
+		case SHOW_INFO_DIALOG:
+			sInfoDialog.setTitle(sComicTitle);
 			sAltTextView.setText(sAltText);
 		default:
 			break;
-    	}
-    }
-	
-    /**
-     * Ensures the string for the image source is a valid url.
-     * 
-     * E.g.: parseImageSource("/comic37.PNG") -> "http://base_url/comic37.PNG"
-     * 
-     * @param rawString  The datum to parse into a proper url. 
-     */
-    private void parseImageSource(String rawString) {
-    	// Check if the image source has the base url on it.
-    	if (!rawString.startsWith(getString(R.string.base_url))) {
-    		// If it doesn't, prepend the string with the base_url,
-    		// Remembering to remove the slash from the raw string.
-    		sImageUrl = getString(R.string.base_url) + rawString.substring(1);
-    	} else {
-    		// The string should already be parsed.
-    		sImageUrl = rawString;
-    	}
-    }
-    
-    /**
-     * Does a whole mess of stuff.
-     * 
-     * Gets the correct comic number, image source, comic title, and alt-text.
-     */
-    private void scrapeSite() {
-    	// TODO: This should probably be refactored to more specific methods.
-    	// It's time for some fun.
-    	try {
-    		// Set the present URL, in case it's never been done.
-    		sPresentUrl = getString(R.string.base_url) + sPresentComicNumber;
-    		// Connect to the site and get the HTML.
+		}
+	}
+
+	/**
+	 * Ensures the string for the image source is a valid url.
+	 * 
+	 * E.g.: parseImageSource("/comic37.PNG") -> "http://base_url/comic37.PNG"
+	 * 
+	 * @param rawString  The datum to parse into a proper url. 
+	 */
+	private void parseImageSource(String rawString) {
+		// Check if the image source has the base url on it.
+		if (!rawString.startsWith(getString(R.string.base_url))) {
+			// If it doesn't, prepend the string with the base_url,
+			// Remembering to remove the slash from the raw string.
+			sImageUrl = getString(R.string.base_url) + rawString.substring(1);
+		} else {
+			// The string should already be parsed.
+			sImageUrl = rawString;
+		}
+	}
+
+	/**
+	 * Does a whole mess of stuff.
+	 * 
+	 * Gets the correct comic number, image source, comic title, and alt-text.
+	 */
+	private void scrapeSite() {
+		// TODO: This should probably be refactored to more specific methods.
+		// It's time for some fun.
+		try {
+			// Set the present URL, in case it's never been done.
+			sPresentUrl = getString(R.string.base_url) + sPresentComicNumber;
+			// Connect to the site and get the HTML.
 			sRawHtml = Jsoup.connect(sPresentUrl).get();
 			// Try to get the div with a post class.
 			Elements divTags = sRawHtml.select(getString(R.string.find_div_tag));
-			
+
 			if (divTags.size() > 0) {
 				// Assuming we got any divs, take the first one.
 				Element divTag = divTags.first();
@@ -294,16 +294,16 @@ public class GooseActivity extends Activity {
 				String presentNumber = divTag.id().substring(5);
 				// Convert it to an integer, so we can play with it.
 				sPresentComicNumber = Integer.parseInt(presentNumber);
-		    	// If this is the first time we've scraped the site,
+				// If this is the first time we've scraped the site,
 				// or the latest button on pressed, set the latest comic number.
-		    	if (!sScraped) {
-		        	sCurrentComicNumber = sPresentComicNumber;
-		        	sScraped = true;
-		    	}
+				if (!sScraped) {
+					sCurrentComicNumber = sPresentComicNumber;
+					sScraped = true;
+				}
 				// Now that we have the actual comic number,
 				// reset the present URL to the actual URL.
 				sPresentUrl = getString(R.string.base_url) + sPresentComicNumber; 
-				
+
 				// Grab the comic header and the img stuff.
 				Elements anchorTags = divTag.select(getString(R.string.find_anchor_tag));
 				Elements imageTags = divTag.select(getString(R.string.find_image_tag));
@@ -316,7 +316,7 @@ public class GooseActivity extends Activity {
 					// There was no title for the comic. Use a default value.
 					sComicTitle = getString(R.string.app_name);
 				}
-				
+
 				// See if we got any image tags.
 				if (imageTags.size() > 0) {
 					// Nab the source of the image and the title.
@@ -332,81 +332,81 @@ public class GooseActivity extends Activity {
 				// Couldn't find a div tag.
 				Toast.makeText(getApplicationContext(), R.string.loading_error, Toast.LENGTH_SHORT).show();
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
-    /**
-     * Creates a share intent and calls it for the user to share a link to the comic.
-     */
-    private void shareComic() {
-    	// Create the intent.
-    	Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
-    	// Let's use just plain text.
-    	shareIntent.setType(getString(R.string.plain_text));
-    	// Create the content.
-    	String comicLink = getString(R.string.share_message) + sPresentUrl;
-    	// Pass it to the intent.
-    	shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, comicLink);
-    	// Call the activity.
-    	startActivity(Intent.createChooser(shareIntent, getString(R.string.share_title)));
-    }
-    
-    /** 
-     * Toggles the comic navigation on the screen.
-     */
-    private void toggleNavigation() {
-    	if (sNavigation == true) {
-    		sNavigationLayout.setVisibility(View.VISIBLE);
-    	} else {
-    		sNavigationLayout.setVisibility(View.GONE);
-    	}
-    }
-    
-    /**
-     * Performs site scraping in a child thread so as not to block the UI thread.
-     * 
-     * @author Hardy Jones III
-     *
-     */
-    private class scrapeSiteTask extends AsyncTask<Void, Void, Void> {
+	/**
+	 * Creates a share intent and calls it for the user to share a link to the comic.
+	 */
+	private void shareComic() {
+		// Create the intent.
+		Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+		// Let's use just plain text.
+		shareIntent.setType(getString(R.string.plain_text));
+		// Create the content.
+		String comicLink = getString(R.string.share_message) + sPresentUrl;
+		// Pass it to the intent.
+		shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, comicLink);
+		// Call the activity.
+		startActivity(Intent.createChooser(shareIntent, getString(R.string.share_title)));
+	}
 
-    	/**
-    	 * Sets up the task.  Creates a progress dialog with loading info.
-    	 * @param mainActivity the activity that is calling
-    	 */
-    	public scrapeSiteTask(GooseActivity mainActivity) {
-    		// Allow a listener to cancel this whole task.
-    		sLoadingComic.setOnCancelListener(new OnCancelListener() {
-			public void onCancel(DialogInterface dialog) {
-				scrapeSiteTask.this.cancel(true);
-			}});
-    	}
+	/** 
+	 * Toggles the comic navigation on the screen.
+	 */
+	private void toggleNavigation() {
+		if (sNavigation == true) {
+			sNavigationLayout.setVisibility(View.VISIBLE);
+		} else {
+			sNavigationLayout.setVisibility(View.GONE);
+		}
+	}
 
-    	
-    	/**
-    	 * Shows the actual progress dialog.
-    	 */
-    	@Override
-    	protected void onPreExecute() {
-        	// Throw up the progress dialog box.
-    		sLoadingComic.show();
-    	}
-    	
-    	/**
-    	 * This is the grunt work.  Does the actual scraping of the site.
-    	 */
+	/**
+	 * Performs site scraping in a child thread so as not to block the UI thread.
+	 * 
+	 * @author Hardy Jones III
+	 *
+	 */
+	private class scrapeSiteTask extends AsyncTask<Void, Void, Void> {
+
+		/**
+		 * Sets up the task.  Creates a progress dialog with loading info.
+		 * @param mainActivity the activity that is calling
+		 */
+		public scrapeSiteTask(GooseActivity mainActivity) {
+			// Allow a listener to cancel this whole task.
+			sLoadingComic.setOnCancelListener(new OnCancelListener() {
+				public void onCancel(DialogInterface dialog) {
+					scrapeSiteTask.this.cancel(true);
+				}});
+		}
+
+
+		/**
+		 * Shows the actual progress dialog.
+		 */
+		@Override
+		protected void onPreExecute() {
+			// Throw up the progress dialog box.
+			sLoadingComic.show();
+		}
+
+		/**
+		 * This is the grunt work.  Does the actual scraping of the site.
+		 */
 		@Override
 		protected Void doInBackground(Void... params) {
 			// Scrape the goose.
 			scrapeSite();
-			
+
 			return null;
 		}
-    	
+
 		/**
 		 * Load whatever image we got into the webview.
 		 */
@@ -415,20 +415,23 @@ public class GooseActivity extends Activity {
 			// Load the comic if nothing was canceled.
 			sComicView.loadUrl(sImageUrl);
 		}
-		
+
 		/**
 		 * Loads old info so that the other features are consistent.
 		 */
 		@Override
 		protected void onCancelled() {
-			// Load the old info back.
-			sPresentComicNumber = oldInfo.getmPresentComicNumber();
-			sCurrentComicNumber = oldInfo.getmLatestComicNumber();
-			sAltText = oldInfo.getmAltText();
-			sComicTitle = oldInfo.getmComicTitle();
-			sImageUrl = oldInfo.getmImageUrl();
-			sPresentUrl = oldInfo.getmPresentUrl();
+			// Ensure we can even get old info.
+			if (null != oldInfo) {
+				// Load the old info back.
+				sPresentComicNumber = oldInfo.getmPresentComicNumber();
+				sCurrentComicNumber = oldInfo.getmLatestComicNumber();
+				sAltText = oldInfo.getmAltText();
+				sComicTitle = oldInfo.getmComicTitle();
+				sImageUrl = oldInfo.getmImageUrl();
+				sPresentUrl = oldInfo.getmPresentUrl();
+			}
 			super.onCancelled();
 		}
-    }
+	}
 }
